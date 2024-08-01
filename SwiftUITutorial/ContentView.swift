284c97d88caae70c68d9641510d39a132ef711db
Jboy422 <gitonly543@gmail.com>
2024-08-02 06:51:19 +0900

    wip

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -1,29 +1,31 @@
 import SwiftUI
 
-// 状態が変わる親のView
 struct ContentView: View {
-    @State private var isPlaying = false
-
+    @State private var isShowingSheet = false
+    
     var body: some View {
-        VStack {
-            // このViewを呼び出して、引数で状態を渡す
-            PlayerView(isPlaying: $isPlaying)
-            Button(action: {
-                isPlaying.toggle()
-            }) {
-                Text(isPlaying ? "⏹️" : "▶️")
-                    .font(.system(size: 100))
+        Button(action: {
+            isShowingSheet.toggle()
+        }) {
+            Text("Show License Agreement")
+        }
+        .sheet(isPresented: $isShowingSheet,
+               onDismiss: didDismiss) {
+            VStack {
+                Text("License Agreement")
+                    .font(.title)
+                    .padding(50)
+                Text("""
+                                Terms and conditions go here.
+                            """)
+                .padding(50)
+                Button("Dismiss",
+                       action: { isShowingSheet.toggle() })
             }
         }
     }
-}
-
-// 子のViewには、@Bindingを定義すると外部から引数で渡せるようになる。
-struct PlayerView: View {
-    @Binding var isPlaying: Bool
-
-    var body: some View {
-        Text(isPlaying ? "🚀" : "💸")
-            .font(.system(size: 100))
-    }
+    
+    func didDismiss() {
+            // Handle the dismissing action.
+        }
 }
